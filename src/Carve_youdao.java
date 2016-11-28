eaa552b1ee733c4b8341e1f72e16fbd4a8d2373d
ymniam-MKorch <1013287904@qq.com>
2016-11-28 22:10:36 +0800

    Crave_youdao1.0

--- src/Carve_youdao.java
+++ src/Carve_youdao.java
@@ -16,8 +16,36 @@ public class Carve_youdao {
 
     public void Carve_youdao(){
 
-        carve_explain = explain;
-
+        carve_explain = "";
+		int index1=explain.indexOf('[');
+		int index2=explain.indexOf(']');
+		carve_explain+=explain.substring(index1+2, index2-1);
+		if (!isChinese(carve_explain)){
+			return;
+		}
+		carve_explain+="\n";
+		int index3=explain.indexOf('[',index1+1);
+		int index4=explain.indexOf(']',index2+1);
+		String temp=explain.substring(index3+1, index4);
+		int index5=-1;
+		int index6=-1;
+		while(index6<temp.length()-1){
+			index5=temp.indexOf('"',index6+1);
+			index6=temp.indexOf('"',index5+1);
+			carve_explain=carve_explain+temp.substring(index5+1, index6)+'\n';
+		}
     }
+    
+    public static boolean isChinese(char c) {
+        return c >= 0x4E00 &&  c <= 0x9FA5;
+  }
+
+  public static boolean isChinese(String str) {
+      if (str == null) return false;
+      for (char c : str.toCharArray()) {
+          if (isChinese(c)) return true;
+      }
+      return false;
+  }
 
 }
